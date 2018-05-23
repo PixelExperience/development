f36b632adb3f41241facb2a0d05690d06efb581a
Diego Wilson <diegowilson@google.com>
2018-05-23 12:11:05 -0700

    Relocate all overlay build artifacts to out
    The convention in Android platform builds is to put all build files in the out
    directory. So relocate build artifacts from the overlay directory there.
    
    Now attempts to write files to any source directory are instead written
    to out/overlays/artifacts.
    
    Bug: 70621515
    Change-Id: I938707fb0b85c3c887a8100c685edd3539aa0387

--- keystone/dockerfile_test.py
+++ keystone/dockerfile_test.py
@@ -4,8 +4,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import unittest
 import subprocess
+import unittest
 
 
 class DockerfileTest(unittest.TestCase):
