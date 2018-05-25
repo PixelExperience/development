ea547841da5fa574eb9bd88f4e442a81ee60ed1e
Diego Wilson <diegowilson@google.com>
2018-05-25 09:50:24 -0700

    Refactor overlay mounts
    Each mount point added by the overlay class is now managed individually
    by the Mount class. This guarantees every mount is unmounted
    when the overlay class is destroyed.
    
    Test: python overlay_test.py
    Bug: 78182692
    Change-Id: Ieb35af9162f6e31ffc407140f9174b90dcae0bc3

--- keystone/overlay_test.py
+++ keystone/overlay_test.py
@@ -4,72 +4,78 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+import shutil
+import subprocess
+import tempfile
 import unittest
-import mock
 import overlay
+
+
+class MountTest(unittest.TestCase):
+
+  def testCreate(self):
+    mount = overlay.Mount(
+        mount_command=['/bin/true'],
+        unmount_command=['/bin/true'])
+    self.assertEqual(
+        mount.mount_command,
+        ['/bin/true']
+    )
+
+  def testFailedMount(self):
+    with self.assertRaises(subprocess.CalledProcessError):
+      overlay.Mount(
+          mount_command=['/bin/false'],
+          unmount_command=['/bin/true']
+      )
+
+  def testDelete(self):
+    tempdir = tempfile.mkdtemp()
+    try:
+      unmount_file = os.path.join(tempdir, 'unmounted')
+      overlay.Mount(
+          mount_command=['/bin/true'],
+          unmount_command=['/bin/touch', unmount_file])
+
+      self.assertTrue(os.path.exists(unmount_file))
+    finally:
+      shutil.rmtree(tempdir)
 
 
 class OverlayTest(unittest.TestCase):
 
-  @mock.patch('overlay.os.rmdir')
-  @mock.patch('overlay.tempfile.mkdtemp', return_value='/tmp/out_dir')
-  @mock.patch('overlay.os.mkdir')
-  @mock.patch('overlay.open')
-  @mock.patch('overlay.subprocess.check_call')
-  def testCreateFromValidTarget(self,
-                                mock_check_call,
-                                mock_open,
-                                mock_mkdir,
-                                mock_mkdtemp,
-                                mock_rmdir):
-    del mock_open, mock_mkdir, mock_mkdtemp, mock_rmdir
-    o = overlay.Overlay(
-        target='sdm845',
-        source_dir='/source_dir')
-    self.assertIsNotNone(o)
-    mock_check_call.assert_has_calls([
-        mock.call([
-            'sudo', 'mount', '--bind', '/source_dir/out', '/tmp/out_dir'
-        ]),
-        mock.call([
-            'sudo', 'mount',
-            '--types', 'overlay',
-            '--options',
-            ','.join([
-                'lowerdir=/source_dir/overlays/qcom-LA.UM.7.3-incoming:'
-                '/source_dir',
-                'upperdir=/source_dir/out/overlays/sdm845/artifacts',
-                'workdir=/source_dir/out/overlays/sdm845/work'
-            ]),
-            'overlay',
-            '/source_dir'
-        ]),
-        mock.call([
-            'sudo', 'mount', '--bind', '/tmp/out_dir', '/source_dir/out'
-        ]),
-    ])
+  class FakeMount(object):
 
-  @mock.patch('overlay.os.rmdir')
-  @mock.patch('overlay.tempfile.mkdtemp', return_value='/tmp/out_dir')
-  @mock.patch('overlay.os.mkdir')
-  @mock.patch('overlay.open')
-  @mock.patch('overlay.subprocess.check_call')
-  def testDeleteFromValidTarget(self,
-                                mock_check_call,
-                                mock_open,
-                                mock_mkdir,
-                                mock_mkdtemp,
-                                mock_rmdir):
-    del mock_open, mock_mkdir, mock_mkdtemp, mock_rmdir
+    def __init__(self, mount_command, unmount_command):
+      self.mount_command = mount_command
+      self.unmount_command = unmount_command
+
+  def setUp(self):
+    overlay.Mount = self.FakeMount
+    self.source_dir = tempfile.mkdtemp()
+    os.mkdir(os.path.join(self.source_dir, 'overlays'))
+    os.mkdir(os.path.join(self.source_dir,
+                          'overlays', 'qcom-LA.UM.7.3-incoming'))
+
+  def tearDown(self):
+    shutil.rmtree(self.source_dir)
+
+  def testCreateFromValidTarget(self):
     o = overlay.Overlay(
         target='sdm845',
-        source_dir='/source_dir')
-    del o
-    mock_check_call.assert_has_calls([
-        mock.call(['sudo', 'umount', '/source_dir/out']),
-        mock.call(['sudo', 'umount', '/tmp/out_dir']),
-        mock.call(['sudo', 'umount', '/source_dir'])
-    ])
+        source_dir=self.source_dir)
+    self.assertIsNotNone(o)
+    mounts = o.GetMountInfo()
+    unmount_commands = [mount['unmount_command'] for mount in mounts]
+    self.assertIn(
+        ['sudo', 'umount', os.path.join(self.source_dir)],
+        unmount_commands
+    )
+    self.assertIn(
+        ['sudo', 'umount', os.path.join(self.source_dir, 'out')],
+        unmount_commands
+    )
 
   def testInvalidTarget(self):
     with self.assertRaises(KeyError):
